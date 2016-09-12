d473ad604af0507d5c5963ef0b9fca43482c50ac
Gonzalo Martin <gonzalomvp@hotmail.com>
2016-09-12 14:27:39 +0000

    Added CEC support with libcec2

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -20,11 +20,26 @@
 #include <sstream>
 #include <boost/locale.hpp>
 
+#include <libcec/cec.h>
+// cecloader.h uses std::cout _without_ including iosfwd or iostream
+// Furthermore is uses cout and not std::cout
+using std::cout;
+using std::endl;
+#include <libcec/cecloader.h>
+
+#include "bcm_host.h"
+//#LINKFLAGS=-lbcm_host
+
+#include <algorithm>
+#include <map>
+
 #ifdef WIN32
 #include <Windows.h>
 #endif
 
 namespace fs = boost::filesystem;
+std::map<std::string, int> key2cec;
+std::map<int, int> cec2sdl;
 
 bool scrape_cmdline = false;
 
@@ -155,6 +170,99 @@ void onExit()
 	Log::close();
 }
 
+//CEC::CBCecKeyPressType 
+int on_keypress(void* not_used, const CEC::cec_keypress msg)
+{
+	if (msg.duration == 0)
+	{
+		if(cec2sdl.find(msg.keycode) == cec2sdl.end())
+		{
+			return 1;
+		}
+		
+		int key_id = cec2sdl[msg.keycode];
+		//std::cout << "on_keypress: " << static_cast<int>(msg.keycode) << " " << key_id << std::endl;
+		
+		SDL_Event* event = new SDL_Event();
+		event->key.keysym.sym = key_id;
+		event->type = SDL_KEYDOWN;
+		SDL_PushEvent(event);
+		event->type = SDL_KEYUP;
+		SDL_PushEvent(event);
+	}
+
+	return 0;
+}
+
+bool loadCECFromXML()
+{
+	//Populate some defaults
+	key2cec["a"] = CEC::CEC_USER_CONTROL_CODE_SELECT;
+	key2cec["b"] = CEC::CEC_USER_CONTROL_CODE_EXIT;
+	key2cec["down"] = CEC::CEC_USER_CONTROL_CODE_DOWN;
+	key2cec["left"] = CEC::CEC_USER_CONTROL_CODE_LEFT;
+	key2cec["right"] = CEC::CEC_USER_CONTROL_CODE_RIGHT;
+	key2cec["up"] = CEC::CEC_USER_CONTROL_CODE_UP;
+	key2cec["pageup"] = CEC::CEC_USER_CONTROL_CODE_F2_RED;
+	key2cec["pagedown"] = CEC::CEC_USER_CONTROL_CODE_F3_GREEN;
+	key2cec["start"] = CEC::CEC_USER_CONTROL_CODE_SETUP_MENU;
+	key2cec["select"] = CEC::CEC_USER_CONTROL_CODE_F4_YELLOW;
+	key2cec["exit"] = CEC::CEC_USER_CONTROL_CODE_CLEAR;
+	
+	cec2sdl[key2cec["a"]]=SDLK_RETURN;
+	cec2sdl[key2cec["b"]]=SDLK_ESCAPE;
+	cec2sdl[key2cec["down"]]=SDLK_DOWN;
+	cec2sdl[key2cec["left"]]=SDLK_LEFT;
+	cec2sdl[key2cec["right"]]=SDLK_RIGHT;
+	cec2sdl[key2cec["up"]]=SDLK_UP;
+	cec2sdl[key2cec["pageup"]]=SDLK_RIGHTBRACKET;
+	cec2sdl[key2cec["pagedown"]]=SDLK_LEFTBRACKET;
+	cec2sdl[key2cec["start"]]=SDLK_F1;
+	cec2sdl[key2cec["select"]]=SDLK_F2;
+
+	std::string path = InputManager::getConfigPath();
+	if(!fs::exists(path))
+		return false;
+	
+	pugi::xml_document doc;
+	pugi::xml_parse_result res = doc.load_file(path.c_str());
+
+	if(!res)
+	{
+		LOG(LogError) << "Error parsing input config: " << res.description();
+		return false;
+	}
+
+	pugi::xml_node root = doc.child("inputList");
+	if(!root)
+		return false;
+
+	pugi::xml_node node = root.find_child_by_attribute("inputConfig", "deviceName", "cec");
+	if(node)
+	{
+		for(pugi::xml_node input = node.child("input"); input; input = input.next_sibling("input"))
+		{
+			std::string name = input.attribute("name").as_string();
+			int id = input.attribute("id").as_int();
+			key2cec[name] = id;
+		}
+		cec2sdl[key2cec["exit"]] = SDLK_F4;
+	}
+
+	node = root.find_child_by_attribute("inputConfig", "deviceName", "Keyboard");
+	if(node)
+	{
+		for(pugi::xml_node input = node.child("input"); input; input = input.next_sibling("input"))
+		{
+			std::string name = input.attribute("name").as_string();
+			int id = input.attribute("id").as_int();
+			cec2sdl[key2cec[name]] = id;
+		}
+	}
+	
+	return true;
+}
+
 int main(int argc, char* argv[])
 {
 	unsigned int width = 0;
@@ -275,6 +383,55 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	//CEC support
+	
+	// Initialise the graphics pipeline for the raspberry pi. Yes, this is necessary.
+	bcm_host_init();
+
+	// Set up the CEC config and specify the keypress callback function
+	CEC::ICECCallbacks        cec_callbacks;
+	CEC::libcec_configuration cec_config;
+	cec_config.Clear();
+	cec_callbacks.Clear();
+
+	const std::string devicename("CECExample");
+	devicename.copy(cec_config.strDeviceName, std::min(devicename.size(),13u) );
+	
+	cec_config.clientVersion       = CEC::CEC_CLIENT_VERSION_CURRENT;
+	cec_config.bActivateSource     = 1;
+	cec_config.callbacks           = &cec_callbacks;
+	cec_config.deviceTypes.Add(CEC::CEC_DEVICE_TYPE_RECORDING_DEVICE);
+
+	cec_callbacks.CBCecKeyPress    = &on_keypress;
+
+	// Get a cec adapter by initialising the cec library
+	CEC::ICECAdapter* cec_adapter = LibCecInitialise(&cec_config);
+	if( !cec_adapter )
+	{ 
+		std::cerr << "Failed loading libcec.so\n"; 
+		return 1; 
+	}
+
+	// Try to automatically determine the CEC devices 
+	CEC::cec_adapter devices[10];
+	int8_t devices_found = cec_adapter->FindAdapters(devices, 10, NULL);
+	if( devices_found <= 0)
+	{
+		std::cerr << "Could not automatically determine the cec adapter devices\n";
+		UnloadLibCec(cec_adapter);
+		return 1;
+	}
+
+	// Open a connection to the zeroth CEC device
+	if( !cec_adapter->Open(devices[0].comm) )
+	{
+		std::cerr << "Failed to open the CEC device on port " << devices[0].comm << std::endl;
+		UnloadLibCec(cec_adapter);
+		return 1;
+	}
+	
+	loadCECFromXML();
+	
 	//generate joystick events since we're done loading
 	SDL_JoystickEventState(SDL_ENABLE);
 
@@ -327,6 +484,10 @@ int main(int argc, char* argv[])
 
 		Log::flush();
 	}
+	
+	// CEC Close down and cleanup
+	cec_adapter->Close();
+	UnloadLibCec(cec_adapter);
 
 	while(window.peekGui() != ViewController::get())
 		delete window.peekGui();
